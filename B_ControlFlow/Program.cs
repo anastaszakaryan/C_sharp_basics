2509ca13d6e21fe13f7dcc4b75cb001a39bb291a
anzakaryan <anzakaryan@ozon.ru>
2023-01-07 23:32:55 +0300

    nested for

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -4,9 +4,43 @@ namespace B_ControlFlow
 {
     class Program
     {
-        static void Main(string[] args)
+        static void NestedFor()
         {
-            
+            int[] numbers = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Pair ({atI};{atJ}), indexes ({i};{j})");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                for (int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < numbers.Length; k++)
+                    {
+                        int atI = numbers[i];
+                        int atJ = numbers[j];
+                        int atK = numbers[k];
+
+                        if (atI + atJ + atK == 0)
+                        {
+                            Console.WriteLine($"Triplets ({atI};{atJ};{atK}), indexes ({i};{j};{k})");
+                        }
+                    }
+                }
+            }
         }
 
         static void ForForeach()
