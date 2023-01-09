ea746aa690a6a2ffa9fe38877feded68a7b08732
anzakaryan <anzakaryan@ozon.ru>
2023-01-09 19:44:23 +0300

    break continue

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -6,6 +6,61 @@ namespace B_ControlFlow
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+
+            int[] numbers = { 0, 3, 2, 1, 5, 4, 6, 7, 8, 9 };
+
+            foreach (int n in numbers)
+            {
+                if (n % 2 != 0)
+                    continue;
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine();
+
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Number={numbers[i]}");
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (numbers[i] == j)
+                        break;
+                    Console.Write($"{letters[j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            int[] numbers2 = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
+
+            int counter = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (counter == 3)
+                    break; // досрочно прерывает цикл
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Pair ({atI};{atJ}), indexes ({i};{j})");
+                        counter++;
+                        if (counter == 3)
+                            break;
+                    }
+                }
+            }
+        }
+        static void WhileDoWhile()
+        {
             int age = 0;
             while (age < 18)
             {
