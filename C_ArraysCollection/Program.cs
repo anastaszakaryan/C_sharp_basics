43e238032d76f5d9a5533de3bebd0a9b842e798a
anzakaryan <anzakaryan@ozon.ru>
2023-01-28 18:13:19 +0300

    list demo

--- C_ArraysCollection/Program.cs
+++ C_ArraysCollection/Program.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace C_ArraysCollection
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            var intList = new List<int>() { 1, 4, 2, 7, 5, 9, 12 }; // обобщение
+            intList.Add(7);
+
+            int[] intArray = { 1, 2, 3 };
+            intList.AddRange(intArray);
+
+            if (intList.Remove(1)) //first occurence
+            {
+                //do
+
+            }
+            else { }
+            intList.RemoveAt(0);
+
+            intList.Reverse();
+
+            bool contains = intList.Contains(3);
+
+            int min = intList.Min();
+            int max = intList.Max();
+
+            Console.WriteLine($"Min={min}. Max={max}");
+
+            int indexOf = intList.IndexOf(2);
+            int lastIndexOf = intList.LastIndexOf(2);
+
+            Console.WriteLine($"IndexOf2={indexOf}. LastIndexOf2={lastIndexOf}");
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Console.Write($"{intList[i]} ");
+            }
+            Console.WriteLine();
+
+            foreach (int item in intList)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+        static void ArrayType()
         {
             int[] a1;
             a1 = new int[10];
