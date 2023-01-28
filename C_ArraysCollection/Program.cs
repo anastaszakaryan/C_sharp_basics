9520825c7d307f83c15b69f2a6ea2df87c9e8bc2
anzakaryan <anzakaryan@ozon.ru>
2023-01-29 01:02:39 +0300

    stack queue

--- C_ArraysCollection/Program.cs
+++ C_ArraysCollection/Program.cs
@@ -8,6 +8,47 @@ namespace C_ArraysCollection
     {
         static void Main(string[] args)
         {
+            // примеры стека: мытьё посуды, отмена операций
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            Console.WriteLine($"Should print out 4: {stack.Peek()}");
+
+            stack.Pop();
+
+            Console.WriteLine($"Should print out 3: {stack.Peek()}");
+
+            Console.WriteLine("Iterate over the stack");
+            foreach (var cur in stack)
+            {
+                Console.WriteLine(cur);
+            }
+
+            //примеры очереди: очередь из людей
+            var queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            Console.WriteLine($"Should print out 1: {queue.Peek()}");
+
+            queue.Dequeue();
+
+            Console.WriteLine($"Should print out 2: {queue.Peek()}");
+
+            Console.WriteLine("Iterate over the queue");
+            foreach (var cur in queue)
+            {
+                Console.WriteLine(cur);
+            }
+
+        }
+        static void Dictionary()
+        {
             var people = new Dictionary<int, string>();
             people.Add(1, "John");
             people.Add(2, "Bob");
