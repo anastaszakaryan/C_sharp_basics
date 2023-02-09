dc915a1fd5bb3f6b84f3743701a1949579d82b35
anzakaryan <anzakaryan@ozon.ru>
2023-02-10 00:48:32 +0300

    multidim arrays

--- C_ArraysCollection/Program.cs
+++ C_ArraysCollection/Program.cs
@@ -8,6 +8,22 @@ namespace C_ArraysCollection
     {
         static void Main(string[] args)
         {
+            int[,] r1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] r2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < r2.GetLength(0); i++)
+            {
+                for (int j = 0; j < r2.GetLength(1); j++)
+                {
+                    Console.Write($"{r2[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        static void StackQueue()
+        {
             // примеры стека: мытьё посуды, отмена операций
             var stack = new Stack<int>();
             stack.Push(1);
