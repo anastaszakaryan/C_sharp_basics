e5d98b613468893df6dac31b432348d669b134f9
anzakaryan <anzakaryan@ozon.ru>
2023-02-10 01:10:32 +0300

    jagged array

--- C_ArraysCollection/Program.cs
+++ C_ArraysCollection/Program.cs
@@ -8,6 +8,35 @@ namespace C_ArraysCollection
     {
         static void Main(string[] args)
         {
+            int[][] jaggedArray = new int[4][];
+            jaggedArray[0] = new int[1];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+            jaggedArray[3] = new int[4];
+
+            Console.WriteLine("Enter the numbers for array");
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    string st = Console.ReadLine();
+                    jaggedArray[i][j] = int.Parse(st);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Printing the Elements");
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void MultidimArrays()
+        {
             int[,] r1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
             int[,] r2 = { { 1, 2, 3 }, { 4, 5, 6 } };
 
