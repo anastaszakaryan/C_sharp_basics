0ba3464f2385f6e2873b0fac4a2a126296d36698
anzakaryan <anzakaryan@ozon.ru>
2023-02-19 21:11:47 +0300

    out modifier

--- D_OOP/D_OOP/Program.cs
+++ D_OOP/D_OOP/Program.cs
@@ -6,6 +6,29 @@ namespace D_OOP
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number, please.");
+            string line = Console.ReadLine();
+            int number;
+            // int number = int.Parse(Console.ReadLine());
+            bool wasParsed = int.TryParse(line, out number);
+            if (wasParsed)
+            {
+                Console.WriteLine(number);
+            }
+            else 
+            {
+                Console.WriteLine("Failed to parse");
+            }
+
+            // Console.WriteLine(number);
+        }
+        static void NamedVariables()
+        {
+            Calculator calc = new Calculator();
+            calc.CalcTriangleSquare(10, 20, 30);
+        }
+        static void Calculation()
+        {
             Calculator calc = new Calculator();
             double avg = calc.Average(new int[] { 1, 2, 3, 4});
             Console.WriteLine(avg);
@@ -14,7 +37,7 @@ namespace D_OOP
             Console.WriteLine(avg);
         }
 
-        static void RomanNumeral()
+        static void RomanNumerals()
         {
             Console.WriteLine(RomanNumeral.Parse("XIV"));
             Console.ReadLine();
